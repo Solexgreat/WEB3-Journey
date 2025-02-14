2991dc80bd09979b31b3988718ece66d9f50f120
Solexgreat <solomongodkid@gmail.com>
2025-02-14 08:17:14 +0100

    Solidity

---
+++ Contracts/fundMe.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {PriceConverter} from "./PriceConverter.sol";
+
+contract fundMe {
+
+    using  PriceConverter for uint256;
+
+    uint256 public miniUSD = 5e18;
+
+    address[] public  fundersAddress;
+
+    mapping(address fundersAddress => uint256 amountFunded) public addressToAmountFunded;
+
+    address public owner;
+
+    constructor () {
+        owner = msg.sender; // deployer of the contract
+    }
+
+    function fund() public payable {
+
+        require(msg.value.getConversion() >= miniUSD, "Didn't send enough ETH");
+        fundersAddress.push(msg.sender);
+        addressToAmountFunded[msg.sender] += msg.value;
+    }
+
+    function withdraw() public onlyOwner {
+        for(uint256 funderIndex = 0; funderIndex < fundersAddress.length; funderIndex++) {
+            address funder =  fundersAddress[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        // // reset the array
+        // fundersAddress = new address[](0);
+        // // withdraw funds
+
+        // // transfer
+        // payable(msg.sender).transfer(address(this).balance);
+
+        // // send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "send failed");
+
+        // //call it been used as a transaction
+        (bool callSuccess, )= payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+    }
+
+    modifier  onlyOwner() {
+        require(msg.sender == owner, "mst be the owner");
+        _;
+    }
+
+  
+
+}
